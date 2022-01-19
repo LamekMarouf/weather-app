0ca75dcb45ed894cfba78babee84de981c67c0be
Lamek Marouf <lamekmaroufjob@hotmail.com>
2022-01-19 17:00:27 +1100

    added day 2-5 of daily forecast

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ let dailyWeatherOne = {
         document.getElementById("forecastTempOne").innerText = "Temp: " + temp + "°C";
         document.getElementById("forecastHumidityOne").innerText = "Humidity: " + humidity + "%";
         document.getElementById("forecastWindOne").innerText = "Wind: " + speed + "km/h";
-        document.getElementById("forecastDateOne").innerText = dt_txt.pop();
+        document.getElementById("forecastDateOne").innerText = dt_txt;
     },
 
     search: function () {
@@ -71,4 +71,154 @@ document.getElementById("searchBar").addEventListener("keyup", function(event) {
         dailyWeatherOne.search();
     }
 })
+
+let dailyWeatherTwo = {
+    "apiKey": "f029df65dad8c980dd6e8213893e1f7e",
+    fetchDailyWeatherTwo: function (dailyCityTwo) {
+        fetch("https://api.openweathermap.org/data/2.5/forecast?q="
+        + dailyCityTwo 
+        + "&units=metric&appid=" 
+        + this.apiKey
+        ).then((response) => response.json())
+        .then((data) => this.displayDailyWeatherTwo(data));
+    },
+    displayDailyWeatherTwo: function(data) {
+        const { icon } = data.list[9].weather[0];
+        const { temp, humidity } = data.list[9].main;
+        const { speed } = data.list[9].wind;
+        const { dt_txt } = data.list[9];
+        document.querySelector(".iconDayTwo").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.getElementById("forecastTempTwo").innerText = "Temp: " + temp + "°C";
+        document.getElementById("forecastHumidityTwo").innerText = "Humidity: " + humidity + "%";
+        document.getElementById("forecastWindTwo").innerText = "Wind: " + speed + "km/h";
+        document.getElementById("forecastDateTwo").innerText = dt_txt;
+    },
+
+    search: function () {
+        this.fetchDailyWeatherTwo(document.getElementById("searchBar").value);
+    }
+};
+
+document.getElementById("searchButton").addEventListener("click", function() {
+    dailyWeatherTwo.search();
+})
+
+document.getElementById("searchBar").addEventListener("keyup", function(event) {
+    if (event.key == "Enter") {
+        dailyWeatherTwo.search();
+    }
+})
+
+let dailyWeatherThree = {
+    "apiKey": "f029df65dad8c980dd6e8213893e1f7e",
+    fetchDailyWeatherThree: function (dailyCityThree) {
+        fetch("https://api.openweathermap.org/data/2.5/forecast?q="
+        + dailyCityThree 
+        + "&units=metric&appid=" 
+        + this.apiKey
+        ).then((response) => response.json())
+        .then((data) => this.displayDailyWeatherThree(data));
+    },
+    displayDailyWeatherThree: function(data) {
+        const { icon } = data.list[9].weather[0];
+        const { temp, humidity } = data.list[9].main;
+        const { speed } = data.list[9].wind;
+        const { dt_txt } = data.list[9];
+        document.querySelector(".iconDayThree").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.getElementById("forecastTempThree").innerText = "Temp: " + temp + "°C";
+        document.getElementById("forecastHumidityThree").innerText = "Humidity: " + humidity + "%";
+        document.getElementById("forecastWindThree").innerText = "Wind: " + speed + "km/h";
+        document.getElementById("forecastDateThree").innerText = dt_txt;
+    },
+
+    search: function () {
+        this.fetchDailyWeatherThree(document.getElementById("searchBar").value);
+    }
+};
+
+document.getElementById("searchButton").addEventListener("click", function() {
+    dailyWeatherThree.search();
+})
+
+document.getElementById("searchBar").addEventListener("keyup", function(event) {
+    if (event.key == "Enter") {
+        dailyWeatherThree.search();
+    }
+})
+
+let dailyWeatherFour = {
+    "apiKey": "f029df65dad8c980dd6e8213893e1f7e",
+    fetchDailyWeatherFour: function (dailyCityFour) {
+        fetch("https://api.openweathermap.org/data/2.5/forecast?q="
+        + dailyCityFour 
+        + "&units=metric&appid=" 
+        + this.apiKey
+        ).then((response) => response.json())
+        .then((data) => this.displayDailyWeatherFour(data));
+    },
+    displayDailyWeatherFour: function(data) {
+        const { icon } = data.list[9].weather[0];
+        const { temp, humidity } = data.list[9].main;
+        const { speed } = data.list[9].wind;
+        const { dt_txt } = data.list[9];
+        document.querySelector(".iconDayFour").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.getElementById("forecastTempFour").innerText = "Temp: " + temp + "°C";
+        document.getElementById("forecastHumidityFour").innerText = "Humidity: " + humidity + "%";
+        document.getElementById("forecastWindFour").innerText = "Wind: " + speed + "km/h";
+        document.getElementById("forecastDateFour").innerText = dt_txt;
+    },
+
+    search: function () {
+        this.fetchDailyWeatherFour(document.getElementById("searchBar").value);
+    }
+};
+
+document.getElementById("searchButton").addEventListener("click", function() {
+    dailyWeatherFour.search();
+})
+
+document.getElementById("searchBar").addEventListener("keyup", function(event) {
+    if (event.key == "Enter") {
+        dailyWeatherFour.search();
+    }
+})
+
+let dailyWeatherFive = {
+    "apiKey": "f029df65dad8c980dd6e8213893e1f7e",
+    fetchDailyWeatherFive: function (dailyCityFive) {
+        fetch("https://api.openweathermap.org/data/2.5/forecast?q="
+        + dailyCityFive 
+        + "&units=metric&appid=" 
+        + this.apiKey
+        ).then((response) => response.json())
+        .then((data) => this.displayDailyWeatherFive(data));
+    },
+    displayDailyWeatherFive: function(data) {
+        const { icon } = data.list[9].weather[0];
+        const { temp, humidity } = data.list[9].main;
+        const { speed } = data.list[9].wind;
+        const { dt_txt } = data.list[9];
+        document.querySelector(".iconDayFive").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.getElementById("forecastTempFive").innerText = "Temp: " + temp + "°C";
+        document.getElementById("forecastHumidityFive").innerText = "Humidity: " + humidity + "%";
+        document.getElementById("forecastWindFive").innerText = "Wind: " + speed + "km/h";
+        document.getElementById("forecastDateFive").innerText = dt_txt;
+    },
+
+    search: function () {
+        this.fetchDailyWeatherFive(document.getElementById("searchBar").value);
+    }
+};
+
+document.getElementById("searchButton").addEventListener("click", function() {
+    dailyWeatherFive.search();
+})
+
+document.getElementById("searchBar").addEventListener("keyup", function(event) {
+    if (event.key == "Enter") {
+        dailyWeatherFive.search();
+    }
+})
+
+
 
